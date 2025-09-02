59ff7030bbab3e73f0183df9e4cbe5ea20a70da6
SEEDART007 <chakrabortysiddhartha58@gmail.com>
2025-09-02 14:57:38 +0530

    updated router fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Educase-Assignment/">
       <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
         <Routes>
           <Route path="/" element={<Welcome />} />
